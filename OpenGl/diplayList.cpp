021c758840951a2f9ebb144758e87daea71766bc
americococo <tmdrb515060@gmail.com>
2019-01-19 01:26:11 +0900

    diplayList를 사용한 보류모드

---
+++ OpenGl/diplayList.cpp
@@ -0,0 +1,44 @@
+#include <glut.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+GLuint MyListID;
+
+void MyCreatList()
+{
+		
+	MyListID = glGenLists(10869);//
+	glNewList(MyListID, GL_COMPILE);
+
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+
+	glEnd();
+	glEndList();
+
+}
+void MyDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glCallList(1);
+	glFlush();
+}
+
+int main(int argc, char ** argv)
+{
+
+	glutInit(&argc, argv);						//glut라이브러리 초기화 & 윈도우 os와 연결하여 하나의 세션 형성
+	glutInitDisplayMode(GLUT_RGB);				//윈도우 기본 컬러 모드를 rgb로 설정
+	glutInitWindowSize(300, 300);				// 폭300 높이 300
+	glutInitWindowPosition(0, 0);				// 윈도우의 운영체제의 화면 좌표계상 0,0에 위치
+	glutCreateWindow("OpenGL Drawing Example");
+	glClearColor(0.0, 0.0, 0.0, 1.0);			//초기화 색깔 지정
+
+	glutDisplayFunc(MyDisplay);
+	MyCreatList();
+	glutMainLoop();
+	return 0;
+}
